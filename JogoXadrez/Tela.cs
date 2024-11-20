5f46d6595f4438e1270195d05cade6282fa94fa2
Sávio Romário <savioromario10@gmail.com>
2024-11-20 16:55:52 -0300

    logica de xeque mate

--- JogoXadrez/Tela.cs
+++ JogoXadrez/Tela.cs
@@ -11,7 +11,21 @@ namespace xadrez_console
             Console.WriteLine();
             imprimirPecasCapturadas(partida);
             Console.WriteLine("Turno: " + partida.turno);
-            Console.WriteLine("Aguardando jogada: " + partida.jogadorAtual);
+
+            if(!partida.terminada)
+            {
+                Console.WriteLine("Aguardando jogada: " + partida.jogadorAtual);
+                if (partida.xeque)
+                {
+                    Console.WriteLine("XEQUE!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("XEQUEMATE!");
+                Console.WriteLine("Partida encerrada!");
+                Console.WriteLine("Ganhador: " + partida.jogadorAtual);
+            }
         }
         public static void imprimirPecasCapturadas(PartidaDeXadrez partida)
         {
